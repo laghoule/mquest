96b518e1be17eebb5c96d53befa9fb729de85913
Pascal Gauthier <pgauthier@nihilisme.ca>
2025-12-23 16:34:14 -0500

    Add sprite erasing routine
    Introduces a new procedure `ERASE_GIRL` to clear the sprite from the
    screen. This routine calculates the memory address of the sprite and
    fills its area with black pixels.

--- anim.asm
+++ anim.asm
@@ -7,6 +7,18 @@ TITLE Animations of a pixel art girl
   ; Girl animations sprite data
   INCLUDE assets/anim.inc
 
+  pos_x DW 150
+  pos_y DW 90
+
+  width_screen DW 320
+  height_screen DW 200
+
+  width_sprite DW 16
+  height_sprite DW 17
+
+  curr_sprite DB g_front_1   ; Front animation for starting point
+  anim_state DB 0            ; 0, 1, 2 (three animations state)
+
 .CODE
 MAIN PROC
   ; Initialize data segment
@@ -32,12 +44,10 @@ MAIN PROC
 
 MAIN ENDP
 
+; --- Game loop ---
 GAME_LOOP PROC
   ; Save registers
-  PUSH AX
-  PUSH BX
-  PUSH CX
-  PUSH DX
+  PUSH AX BX CX DX SI DI
 
 NEXT_KEY:
   ; Get keyboard input
@@ -62,7 +72,7 @@ NEXT_KEY:
 
   CMP AH, 50h       ; Down arrow key
   JE DOWN_DIRECTION
-  
+
   JMP NEXT_KEY      ; Other key press, ignore
 
 RIGHT_DIRECTION:
@@ -73,12 +83,42 @@ DOWN_DIRECTION:
 EXIT_GAME:
 
   ; Restore registers
-  POP DX
-  POP CX
-  POP BX
-  POP AX
-  
+  POP AX BX CX DX SI DI
   RET
 GAME_LOOP ENDP
+
+; -- Erase girl animation --
+ERASE_GIRL PROC
+  ; Save registers
+  PUSH AX BX CX DX SI DI
+
+  ; Clear direction flag
+  CLD
+
+  ; Calcul DI = (pos_y * 320) + pos_x
+  ; Memory address of the sprite
+  MOV AX, pos_y
+  MUL width_screen      ; This can be optimized (costly on 8086)
+  ADD AX, pos_x
+  MOV DI, AX            ; First pixel of the sprite
+
+  MOV AL, 00h           ; Black color (screen background)
+  MOV DX, height_sprite
+
+ERASE_LINE:
+  PUSH DI                ; Save DI (begin of line)
+  MOV  CX, width_sprite  ; Width of the sprite
+  REP  STOSB             ; Fill the line with black pixels (MOV ES:DI AL | INC DI | DEC CX)
+  POP  DI                ; Restore DI (begin of line)
+
+  ADD DI, width_screen   ; Move to next line
+  DEC DX                 ; Decrement height
+  
+  JNZ ERASE_LINE         ; If height > 0, repeat
+
+  ; Restore registers
+  POP AX BX CX DX SI DI
+  RET
+ERASE_GIRL
 
 END MAIN
