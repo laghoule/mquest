e072135b702fe86793676092079f28f6f4d6979d
Pascal Gauthier <pgauthier@nihilisme.ca>
2025-12-29 19:04:36 -0500

    Rename sprite labels from `m_*` to `mia_*` consistently

--- src/main.asm
+++ src/main.asm
@@ -10,14 +10,14 @@ INCLUDE defs/consts.inc ; Constants
 
   INCLUDE assets/mia.inc ; Mia animations sprite data
 
-  m_pos_x DW 150 ; Main caracter initial X position
-  m_pos_y DW 90  ; Main caracter initial Y position
+  mia_pos_x DW 150 ; Main caracter initial X position
+  mia_pos_y DW 90  ; Main caracter initial Y position
 
-  m_curr_sprite   DW OFFSET m_down_0    ; Front / down animation for starting point
-  m_r_anim_state  DB 0                  ; Main caracter right animation state (0, 1, 2 state)
-  m_l_anim_state  DB 0                  ; Main caracter left animation state (0, 1, 2 state)
-  m_u_anim_state  DB 0                  ; Main caracter up animation state (0, 1, 2 state)
-  m_d_anim_state  DB 0                  ; Main caracter down animation state (0, 1, 2 state)
+  mia_curr_sprite   DW OFFSET mia_down_0  ; Front / down animation for starting point
+  mia_r_anim_state  DB 0                  ; Main caracter right animation state (0, 1, 2 state)
+  mia_l_anim_state  DB 0                  ; Main caracter left animation state (0, 1, 2 state)
+  mia_u_anim_state  DB 0                  ; Main caracter up animation state (0, 1, 2 state)
+  mia_d_anim_state  DB 0                  ; Main caracter down animation state (0, 1, 2 state)
 
 .CODE
 MAIN PROC
@@ -83,137 +83,137 @@ read_key_pressed:
     CALL DRAW_CARACTER
     JMP get_next_key
 
-; --- Main caracter move to right ---
+; --- Mia move to right ---
 right_direction:
   CALL ERASE_CARACTER       ; Erase the caracter from the screen
 
-  INC m_pos_x               ; Move the caracter to the right
-  INC m_r_anim_state        ; Increment the animation state
+  INC mia_pos_x             ; Move the caracter to the right
+  INC mia_r_anim_state      ; Increment the animation state
 
-  CMP m_r_anim_state, 3     ; If the animation state is 3, reset it to 0 (3 states)
+  CMP mia_r_anim_state, 3   ; If the animation state is 3, reset it to 0 (3 states)
   JNE @F
-  MOV m_r_anim_state, 0
+  MOV mia_r_anim_state, 0
 
 @@:
-  CMP m_r_anim_state, 0
-  JE m_r_anim_state_0
+  CMP mia_r_anim_state, 0
+  JE mia_r_anim_state_0
 
-  CMP m_r_anim_state, 1
-  JE m_r_anim_state_1
+  CMP mia_r_anim_state, 1
+  JE mia_r_anim_state_1
 
-  MOV m_curr_sprite, OFFSET m_right_2  ; If the animation state is 2, draw the third sprite
+  MOV mia_curr_sprite, OFFSET mia_right_2  ; If the animation state is 2, draw the third sprite
   JMP draw_r_caracter
 
-m_r_anim_state_0:
-  MOV m_curr_sprite, OFFSET m_right_0  ; If the animation state is 0, draw the first sprite
+mia_r_anim_state_0:
+  MOV mia_curr_sprite, OFFSET mia_right_0  ; If the animation state is 0, draw the first sprite
   JMP draw_r_caracter
 
-m_r_anim_state_1:
-  MOV m_curr_sprite, OFFSET m_right_1  ; If the animation state is 1, draw the second sprite
+mia_r_anim_state_1:
+  MOV mia_curr_sprite, OFFSET mia_right_1  ; If the animation state is 1, draw the second sprite
   JMP draw_r_caracter
 
 draw_r_caracter:
-  CALL DRAW_CARACTER     ; Draw the caracter on the screen
-  JMP get_next_key           ; Wait for next key press
+  CALL DRAW_CARACTER      ; Draw the caracter on the screen
+  JMP get_next_key        ; Wait for next key press
 
-; --- Main caracter move to left ---
+; --- Mia move to left ---
 left_direction:
-  CALL ERASE_CARACTER    ; Erase the caracter from the screen
+  CALL ERASE_CARACTER     ; Erase the caracter from the screen
 
-  DEC m_pos_x            ; Move the caracter to the left
-  INC m_l_anim_state     ; Increment the animation state
+  DEC mia_pos_x           ; Move the caracter to the left
+  INC mia_l_anim_state    ; Increment the animation state
 
-  CMP m_l_anim_state, 3  ; If the animation state is 3, reset it to 0 (3 states)
+  CMP mia_l_anim_state, 3 ; If the animation state is 3, reset it to 0 (3 states)
   JNE @F
-  MOV m_l_anim_state, 0
+  MOV mia_l_anim_state, 0
 
 @@:
-  CMP m_l_anim_state, 0
-  JE m_l_anim_state_0
+  CMP mia_l_anim_state, 0
+  JE mia_l_anim_state_0
 
-  CMP m_l_anim_state, 1
-  JE m_l_anim_state_1
+  CMP mia_l_anim_state, 1
+  JE mia_l_anim_state_1
 
-  MOV m_curr_sprite, OFFSET m_left_2  ; If the animation state is 2, draw the third sprite
+  MOV mia_curr_sprite, OFFSET mia_left_2  ; If the animation state is 2, draw the third sprite
   JMP draw_l_caracter
 
-m_l_anim_state_0:
-  MOV m_curr_sprite, OFFSET m_left_0  ; If the animation state is 0, draw the first sprite
+mia_l_anim_state_0:
+  MOV mia_curr_sprite, OFFSET mia_left_0  ; If the animation state is 0, draw the first sprite
   JMP draw_l_caracter
 
-m_l_anim_state_1:
-  MOV m_curr_sprite, OFFSET m_left_1  ; If the animation state is 1, draw the second sprite
+mia_l_anim_state_1:
+  MOV mia_curr_sprite, OFFSET mia_left_1  ; If the animation state is 1, draw the second sprite
   JMP draw_l_caracter
 
 draw_l_caracter:
-  CALL DRAW_CARACTER     ; Draw the caracter on the screen
-  JMP get_next_key           ; Wait for next key press
+  CALL DRAW_CARACTER      ; Draw the caracter on the screen
+  JMP get_next_key        ; Wait for next key press
 
-; --- Main caracter move up ---
+; --- Mia move up ---
 up_direction:
-  CALL ERASE_CARACTER    ; Erase the caracter from the screen
+  CALL ERASE_CARACTER     ; Erase the caracter from the screen
 
-  DEC m_pos_y            ; Move the caracter up
-  INC m_u_anim_state     ; Increment the animation state
+  DEC mia_pos_y           ; Move the caracter up
+  INC mia_u_anim_state    ; Increment the animation state
 
-  CMP m_u_anim_state, 3  ; If the animation state is 3, reset it to 0
+  CMP mia_u_anim_state, 3 ; If the animation state is 3, reset it to 0
   JNE @F
-  MOV m_u_anim_state, 0
+  MOV mia_u_anim_state, 0
 
 @@:
-  CMP m_u_anim_state, 0
-  JE m_u_anim_state_0
+  CMP mia_u_anim_state, 0
+  JE mia_u_anim_state_0
 
-  CMP m_u_anim_state, 1
-  JE m_u_anim_state_1
+  CMP mia_u_anim_state, 1
+  JE mia_u_anim_state_1
 
-  MOV m_curr_sprite, OFFSET m_up_2  ; If the animation state is 2, draw the third sprite
+  MOV mia_curr_sprite, OFFSET mia_up_2  ; If the animation state is 2, draw the third sprite
   JMP draw_u_caracter
 
-m_u_anim_state_0:
-  MOV m_curr_sprite, OFFSET m_up_0  ; If the animation state is 0, draw the first sprite
+mia_u_anim_state_0:
+  MOV mia_curr_sprite, OFFSET mia_up_0  ; If the animation state is 0, draw the first sprite
   JMP draw_u_caracter
 
-m_u_anim_state_1:
-  MOV m_curr_sprite, OFFSET m_up_1  ; If the animation state is 1, draw the second sprite
+mia_u_anim_state_1:
+  MOV mia_curr_sprite, OFFSET mia_up_1  ; If the animation state is 1, draw the second sprite
   JMP draw_u_caracter
 
 draw_u_caracter:
-  CALL DRAW_CARACTER     ; Draw the caracter on the screen
-  JMP get_next_key           ; Wait for next key press
+  CALL DRAW_CARACTER       ; Draw the caracter on the screen
+  JMP get_next_key         ; Wait for next key press
 
-; --- Main caracter move down ---
+; --- Mia move down ---
 down_direction:
-  CALL ERASE_CARACTER    ; Erase the caracter from the screen
+  CALL ERASE_CARACTER      ; Erase the caracter from the screen
 
-  INC m_pos_y            ; Move the caracter down
-  INC m_d_anim_state     ; Increment the animation state
+  INC mia_pos_y            ; Move the caracter down
+  INC mia_d_anim_state     ; Increment the animation state
 
-  CMP m_d_anim_state, 3  ; If the animation state is 3, reset it to 0
+  CMP mia_d_anim_state, 3  ; If the animation state is 3, reset it to 0
   JNE @F
-  MOV m_d_anim_state, 0
+  MOV mia_d_anim_state, 0
 
 @@:
-  CMP m_d_anim_state, 0
-  JE m_d_anim_state_0
+  CMP mia_d_anim_state, 0
+  JE mia_d_anim_state_0
 
-  CMP m_d_anim_state, 1
-  JE m_d_anim_state_1
+  CMP mia_d_anim_state, 1
+  JE mia_d_anim_state_1
 
-  MOV m_curr_sprite, OFFSET m_down_2  ; If the animation state is 2, draw the third sprite
+  MOV mia_curr_sprite, OFFSET mia_down_2  ; If the animation state is 2, draw the third sprite
   JMP draw_d_caracter
 
-m_d_anim_state_0:
-  MOV m_curr_sprite, OFFSET m_down_0  ; If the animation state is 0, draw the first sprite
+mia_d_anim_state_0:
+  MOV mia_curr_sprite, OFFSET mia_down_0  ; If the animation state is 0, draw the first sprite
   JMP draw_d_caracter
 
-m_d_anim_state_1:
-  MOV m_curr_sprite, OFFSET m_down_1  ; If the animation state is 1, draw the second sprite
+mia_d_anim_state_1:
+  MOV mia_curr_sprite, OFFSET mia_down_1  ; If the animation state is 1, draw the second sprite
   JMP draw_d_caracter
 
 draw_d_caracter:
   CALL DRAW_CARACTER     ; Draw the sprite
-  JMP get_next_key           ; Wait for next key press
+  JMP get_next_key       ; Wait for next key press
 
 exit_game:
   RESTORE_REGS
