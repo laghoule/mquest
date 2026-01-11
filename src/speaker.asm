b749c16b4684388f4dca23b49250b0af428a3c85
Pascal Gauthier <pgauthier@nihilisme.ca>
2026-01-10 22:03:23 -0500

    Add GPL copyright headers to source files

--- src/speaker.asm
+++ src/speaker.asm
@@ -1,3 +1,8 @@
+;  Copyright (C) 2025, 2026 Pascal Gauthier
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License.
+
 ; -----------------------------------------------------------------
 ; INIT_MUSIC_THEME
 ; Description: Initializes the music theme by setting the initial
--- src/timer.asm
+++ src/timer.asm
@@ -1,3 +1,8 @@
+;  Copyright (C) 2025, 2026 Pascal Gauthier
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License.
+
 ; ------------------------------------------
 ; INIT_TICKS
 ; Initialize the game tick and pending tick
