938a3c24b9e992609c59a797ccceaddcb818b146
Pascal Gauthier <pgauthier@nihilisme.ca>
2026-02-02 12:52:51 -0500

    Add comments to LOAD_FILE proc

--- src/sys/file.asm
+++ src/sys/file.asm
@@ -13,39 +13,42 @@
 LOAD_FILE PROC
   SAVE_REGS
 
-  MOV AH, 3Dh       ; Open File
-  MOV AL, 0         ; In read only mode
-  INT 21h           ; Call DOS
-  JC @lf_fail_open  ; Carry flag if error
+  ; --- Call DOS for opening the file specified in DX ---
+  MOV AH, 3Dh             ; Open File fucntion
+  MOV AL, 0               ; Open in read only mode
+  INT 21h                 ; Call DOS, return file handle in AX
+  JC @lf_fail_open        ; Carry flag set if error
 
-  MOV BX, AX        ; Store file handle in BX
+  MOV BX, AX              ; Store file handle in BX
 
-  PUSH DX
-  MOV AH, 3Fh       ; Read file
-  MOV CX, 0FFFFh    ; Max allowed 64K
-  MOV DX, DI        ; Destination buffer
-  INT 21h           ; Call DOS
-  POP DX
-  PUSHf             ; Save flags
+  ; --- Call DOS for reading the file ---
+  PUSH DX                 ; Save the file name on the stack
+  MOV AH, 3Fh             ; Read file function
+  MOV CX, 0FFFFh          ; Max allowed 64K (entire segment)
+  MOV DX, DI              ; Destination buffer (DI addr in DX)
+  INT 21h                 ; Call DOS, fill DX (buffered var)
+  POP DX                  ; Restore the file name from the stack
+  PUSHf                   ; Save flags for checking error later, need to close the file before
 
-  MOV AH, 3Eh       ; Close file
-  INT 21h           ; Call DOS
-  POPf              ; Restore flags
-  JC @lf_fail_read  ; Carry flag if error
+  ; -- Call DOS to close the file ---
+  MOV AH, 3Eh             ; Close file function
+  INT 21h                 ; Call DOS
+  POPf                    ; Restore flags from reading function
+  JC @lf_fail_read        ; Carry flag set if error reading file
 
   RESTORE_REGS
   RET
 
-@lf_fail_open:
-  LEA DX, ERR_FILE_OPEN
-  CALL PRINT_ERR
-  STC
+@lf_fail_open:            ; Open file error handler
+  LEA DX, ERR_FILE_OPEN   ; Offset of open file error message
+  CALL PRINT_ERR          ; Call print error
+  STC                     ; Set carry flag to indicate error
   JMP @lf_return
 
-@lf_fail_read:
-  LEA DX, ERR_FILE_READ
-  CALL PRINT_ERR
-  STC
+@lf_fail_read:            ; Read file error handler
+  LEA DX, ERR_FILE_READ   ; Offset of read file error message
+  CALL PRINT_ERR          ; Call print error
+  STC                     ; Set carry flag to indicate error
   JMP @lf_return
 
 @lf_return:
