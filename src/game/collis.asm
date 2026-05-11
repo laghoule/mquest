ef55fada99f74510a5acff1a949b65a7bf897008
Pascal Gauthier <pgauthier@nihilisme.ca>
2026-05-10 21:44:42 -0400

    Add character-to-character AABB collision detection
    - Rename CHECK_COLLISION to CHECK_TILE_COLLISION
    - Rename CHECK_HITBOX_COLLISION to CHECK_OBJECT_COLLISION
    - Add CHECK_CHAR_COLLISION to check a character against all others
    - Add CHECK_CHAR_BOUNDS_COLLISION to perform AABB overlap test
      between two characters on the same scene

--- src/game/collis.asm
+++ src/game/collis.asm
@@ -3,16 +3,15 @@
 ;  it under the terms of the GNU General Public License as published by
 ;  the Free Software Foundation, either version 3 of the License.
 
-;----------------------------------------------------------------
-; CHECK_COLLISION
-; Description: Checks if a position is colliding with an object
+;---------------------------------------------------------------
+; CHECK_TILE_COLLISION
+; Description: Check if collision with tile at position (AX, BX)
 ; Register: AL, DX
 ; Input: AX = pos_x, BX = pos_y
-;   Implicit: curr_scne (via GET_TILE_PROP)
 ; Output: Carry flag set if collision, clear otherwise
 ; Modifed: Carry flag
-;----------------------------------------------------------------
-CHECK_COLLISION PROC
+;---------------------------------------------------------------
+CHECK_TILE_COLLISION PROC
   SAVE_REGS
   CLC                         ; Clear carry flag
 
@@ -39,21 +38,20 @@ CHECK_COLLISION PROC
 @cc_done:
   RESTORE_REGS
   RET
-CHECK_COLLISION ENDP
+CHECK_TILE_COLLISION ENDP
 
 ; ------------------------------------------------------------------------
-; CHECK_HITBOX_COLLISION
+; CHECK_OBJECT_COLLISION
 ; Description: Check if a character hitbox collides with object in the map
 ; Registers: AX, BX, CX, DX, SI, DI
 ; Input: AX = Character index, CX = X position, DX = Y position
-;   Implicit: curr_scne
 ; Output: Carry flag = 1 if collision detected
 ; Modified: Carry flag
 ; ------------------------------------------------------------------------
-CHECK_HITBOX_COLLISION PROC
+CHECK_OBJECT_COLLISION PROC
   SAVE_REGS
 
-  SHL AX, 1                                 ; Conversion characted index -> offset (DW)
+  SHL AX, 1                                 ; Character ID * 2 (word)
   MOV BX, AX
 
   MOV BX, [char_data_table + BX]            ; BX = Address of the character data structure
@@ -79,7 +77,7 @@ CHECK_HITBOX_COLLISION PROC
   MOV BL, [DI].HITBOX.hb_y1
   ADD BX, DX                                ; Add hitbox P1Y to Y position
 
-  CALL CHECK_COLLISION                      ; Check for collition , carry flag will be set if collision
+  CALL CHECK_TILE_COLLISION                 ; Check for collision , carry flag will be set if collision
   JC @chc_return                            ; If collision, return
 
   ; Hitbox 2 position X (CX -> AX (X))
@@ -92,9 +90,172 @@ CHECK_HITBOX_COLLISION PROC
   MOV BL, [DI].HITBOX.hb_y2
   ADD BX, DX                                ; Add hitbox P2Y to Y position
 
-  CALL CHECK_COLLISION                      ; Check for collition, carry flag will be set if collision
+  CALL CHECK_TILE_COLLISION                 ; Check for collition, carry flag will be set if collision
 
 @chc_return:
   RESTORE_REGS
   RET
-CHECK_HITBOX_COLLISION ENDP
+CHECK_OBJECT_COLLISION ENDP
+
+; ------------------------------------------------------------------------
+; CHECK_CHAR_COLLISION
+; Description: Check for character collision with other charater
+; Registers: AX, BX, CX, DX, SI, DI
+; Input: AX = Character ID
+; Output: Carry flag set if collision
+; Modified: aabb_ch1_left, aabb_ch1_right, aabb_ch1_top, aabb_ch1_bottom
+; ------------------------------------------------------------------------
+CHECK_CHAR_COLLISION PROC
+  SAVE_REGS
+
+  SHL AX, 1                                 ; Character ID * 2 (word)
+  MOV BX, AX
+  MOV BX, [char_data_table + BX]            ; BX = Address of the character data structure
+
+  ; TODO: This is a copy/paste of CHECK_HITBOX_COLLISION
+  ;Resolve the direction indirection: Master table -> direction data block
+  MOV SI, [BX].CHARACTER.ch_dir_tbl_addr    ; SI = Address of the 4-directions table
+  XOR DH, DH
+  MOV DL, [BX].CHARACTER.ch_dir
+  SHL DX, 1                                 ; DX = Direction * 2 (word)
+  ADD SI, DX                                ; SI = Address of the direction data
+  MOV SI, [SI]                              ; Dereference the direction data (hitbox, sprites, ...)
+  MOV DI, [SI].CHAR_DIR_DATA.cd_hitbox      ; Dereference the hitbox
+  ;-----------------------------------
+
+  ; --- character 1 left ---
+  XOR CH, CH
+  MOV CL, [DI].HITBOX.hb_x1
+  MOV aabb_ch1_left, CX
+  MOV CX, [BX].CHARACTER.ch_loc.lo_x
+  ADD aabb_ch1_left, CX                     ; aabb_ch1_left =  pos x + hb_x1
+
+  ; --- character 1 right ---
+  XOR CH, CH
+  MOV CL, [DI].HITBOX.hb_x2
+  MOV aabb_ch1_right, CX
+  MOV CX, [BX].CHARACTER.ch_loc.lo_x
+  ADD aabb_ch1_right, CX                    ; aabb_ch1_right = pos x + hb_x2
+
+  ; --- character 1 top ---
+  XOR CH, CH
+  MOV CL, [DI].HITBOX.hb_y1
+  MOV aabb_ch1_top, CX
+  MOV CX, [BX].CHARACTER.ch_loc.lo_y
+  ADD aabb_ch1_top, CX                      ; aabb_ch1_top = pos y + hb_y1
+
+  ; --- character 1 bottom ---
+  XOR CH, CH
+  MOV CL, [DI].HITBOX.hb_y2
+  MOV aabb_ch1_bottom, CX
+  MOV CX, [BX].CHARACTER.ch_loc.lo_y
+  ADD aabb_ch1_bottom, CX                   ; aabb_ch1_bottom = pos y + hb_y2
+
+  MOV CX, 2  ; 2 characters in the game for now | TODO: calc this dynamically
+
+@ccc_next_char:
+  MOV BX, CX ; TODO: This is a hack to avoid a bug
+  DEC BX     ; Check to replace this with another strategie
+  SHL BX, 1
+
+  CMP BX, AX
+  JE @ccc_skip
+
+  MOV BX, [char_data_table + BX]            ; BX = Address of the character data structure
+
+  CALL CHECK_CHAR_BOUNDS_COLLISION
+  JC @ccc_return
+
+@ccc_skip:
+  LOOP @ccc_next_char
+
+
+@ccc_return:
+  RESTORE_REGS
+  RET
+CHECK_CHAR_COLLISION ENDP
+
+; ------------------------------------------------------------------------
+; CHECK_CHAR_BOUNDS_COLLISION
+; Description: Check for collision between the current character and the
+;              character at address BX
+; Registers: AX, BX, CX, DX, SI, DI
+; Input: BX = Address of the character data structure
+; Output: Carry flag set if collision
+; Modified: aabb_ch2_left, aabb_ch2_right, aabb_ch2_top, aabb_ch2_bottom
+; ------------------------------------------------------------------------
+CHECK_CHAR_BOUNDS_COLLISION PROC
+  SAVE_REGS
+
+  ; Are we on the same scene?
+  MOV DX, current_scene_addr
+  CMP [BX].CHARACTER.ch_scene_addr, DX
+  JNE @ccbc_no_collision
+
+  ; TODO: This is a copy/paste of CHECK_HITBOX_COLLISION
+  ;Resolve the direction indirection: Master table -> direction data block
+  MOV SI, [BX].CHARACTER.ch_dir_tbl_addr    ; SI = Address of the 4-directions table
+  XOR DH, DH
+  MOV DL, [BX].CHARACTER.ch_dir
+  SHL DX, 1                                 ; DX = Direction * 2 (word)
+  ADD SI, DX                                ; SI = Address of the direction data
+  MOV SI, [SI]                              ; Dereference the direction data (hitbox, sprites, ...)
+  MOV DI, [SI].CHAR_DIR_DATA.cd_hitbox      ; Dereference the hitbox
+  ;-----------------------------------
+
+  ; --- character 2 left ---
+  XOR DH, DH
+  MOV DL, [DI].HITBOX.hb_x1
+  MOV aabb_ch2_left, DX
+  MOV DX, [BX].CHARACTER.ch_loc.lo_x
+  ADD aabb_ch2_left, DX                     ; aabb_ch2_left = pos x + hb_x1
+
+  MOV DX, aabb_ch2_left
+  CMP aabb_ch1_right, DX                    ; if aabb_ch1_right <= aabb_ch2_left
+  JL @ccbc_no_collision                     ; then no collision
+
+  ; --- character 2 right ---
+  XOR DH, DH
+  MOV DL, [DI].HITBOX.hb_x2
+  MOV aabb_ch2_right, DX
+  MOV DX, [BX].CHARACTER.ch_loc.lo_x
+  ADD aabb_ch2_right, DX                    ; aabb_ch2_right = pos x + hb_x2
+
+  MOV DX, aabb_ch2_right
+  CMP aabb_ch1_left, DX                     ; if aabb_ch1_left >= aabb_ch2_right
+  JG @ccbc_no_collision                     ; then no collision
+
+  ; --- character 2 top ---
+  XOR DH, DH
+  MOV DL, [DI].HITBOX.hb_y1
+  MOV aabb_ch2_top, DX
+  MOV DX, [BX].CHARACTER.ch_loc.lo_y
+  ADD aabb_ch2_top, DX                      ; aabb_ch2_top = pos y + hb_y1
+
+  MOV DX, aabb_ch2_top
+  CMP aabb_ch1_bottom, DX                   ; if aabb_ch1_bottom <= aabb_ch2_top
+  JL @ccbc_no_collision                     ; then no collision
+
+  ; --- character 2 bottom ---
+  XOR DH, DH
+  MOV DL, [DI].HITBOX.hb_y2
+  MOV aabb_ch2_bottom, DX
+  MOV DX, [BX].CHARACTER.ch_loc.lo_y
+  ADD aabb_ch2_bottom, DX                   ; aabb_ch2_bottom = pos y + hb_y2
+
+  MOV DX, aabb_ch2_bottom
+  CMP aabb_ch1_top, DX                      ; if aabb_ch1_top >= aabb_ch2_bottom
+  JG @ccbc_no_collision                     ; then no collision
+
+  ; --- Collision detected ---
+  STC                                       ; Collision detected, set carry flag
+  JMP @ccbc_return
+
+  ; --- No collision detected ---
+@ccbc_no_collision:
+  CLC                                       ; No collision detected, clear carry flag
+
+@ccbc_return:
+  RESTORE_REGS
+  RET
+CHECK_CHAR_BOUNDS_COLLISION ENDP
