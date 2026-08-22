772286387cf6aa8220eda160245bb092d9227936
Pascal Gauthier <pgauthier@nihilisme.ca>
2026-08-22 09:11:11 -0400

    Add tilemap and tileset assets for game map

---
+++ raw/map/basictiles.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="basictiles" tilewidth="16" tileheight="16" tilecount="120" columns="8">
+ <image source="basictiles.png" width="128" height="240"/>
+ <tile id="28">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ raw/map/set_0.tsx
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="set_0" tilewidth="16" tileheight="16" tilecount="324" columns="18">
+ <image source="set_0.png" width="288" height="288"/>
+ <tile id="54">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="126">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="127">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="attr" type="int" value="1"/>
+  </properties>
+ </tile>
+</tileset>
