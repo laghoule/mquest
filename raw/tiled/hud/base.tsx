860013a1bdf9a5ad9bd23bf39423f25a4c7c37d9
Pascal Gauthier <pgauthier@nihilisme.ca>
2026-08-22 20:33:51 -0400

    Reorganize Tiled assets into hud and map subdirectories
    Move map assets from raw/map to raw/tiled/map and create
    new raw/tiled/hud directory with HUD tileset files.

---
+++ raw/tiled/hud/base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="base" tilewidth="16" tileheight="16" tilecount="48" columns="12">
+ <image source="b_pal.png" width="192" height="64"/>
+</tileset>
